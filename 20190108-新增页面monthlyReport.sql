edf7423368bcaa552cf50b8075c1b65c707f1811
HUYANG001 <44154737+HUYANG001@users.noreply.github.com>
2019-01-09 18:12:29 +0800

    Update 20190108-新增页面monthlyReport.sql

--- 20190108-新增页面monthlyReport.sql
+++ 20190108-新增页面monthlyReport.sql
@@ -2,3 +2,7 @@
 insert into function_info (FUNCID, FUNCNAME, PAGEPATH, LOCATION, ISDIRECTORY, LASTDIRECTORY, SHOWSEQ, FUNC_CLASS, FUNC_TYPE, WORKFLOW_FLAG, UP_FUNC_CODE, FUNC_DESC, STATUS, EFFECT_DATE, EXPIRE_DATE, TIMESTAMPS, MISCFLGS, MISC, ICON_CLS)
 values ('1505', 'Monthly Report All Branches', '/fpages/export/form/ftl/M300TabNvalueSCount.ftl', 2, 0, '15', 5, '1', '1', null, null, null, '1', null, null, null, null, null, null);
 
+
+
+insert into Data_Dic (ID, DATA_TYPE_NO, DATA_NO, DATA_TYPE_NAME, DATA_NO_LEN, DATA_NAME, LIMIT_FLAG, HIGH_LIMIT, LOW_LIMIT, EFFECT_DATE, EXPIRE_DATE, TIMESTAMPS, MISCFLGS, APPROVE_STATUS, APPROVE_RESULT, REC_STATUS, REP_STATUS, IS_SUB_SUCCESS, CRT_TM, LST_UPD_TM, LST_UPD_TLR, APPTYPE, BR_NO, YWDATE, ORGCODE, RECORD_UPD_TLR, RECORD_UPD_TM, ST)
+values (214, 20000, 'CPD', '部门', null, 'CPD', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, '1');
