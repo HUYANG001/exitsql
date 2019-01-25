fa31051daeb57a168e78be165b8a91ae78482775
HUYANG001 <44154737+HUYANG001@users.noreply.github.com>
2019-01-25 16:32:46 +0800

    prd2
    1

---
+++ 20190108-新增页面monthlyReport.sql
@@ -0,0 +1,4 @@
+
+insert into function_info (FUNCID, FUNCNAME, PAGEPATH, LOCATION, ISDIRECTORY, LASTDIRECTORY, SHOWSEQ, FUNC_CLASS, FUNC_TYPE, WORKFLOW_FLAG, UP_FUNC_CODE, FUNC_DESC, STATUS, EFFECT_DATE, EXPIRE_DATE, TIMESTAMPS, MISCFLGS, MISC, ICON_CLS)
+values ('1506', 'Monthly Report All Branches', '/fpages/export/form/ftl/M300TabNvalueSCount.ftl', 2, 0, '15', 6, '1', '1', null, null, null, '1', null, null, null, null, null, null);
+
