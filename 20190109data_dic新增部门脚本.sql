0adbe2d505f068bf14f2e11bdbb75267c23a7097
HUYANG001 <44154737+HUYANG001@users.noreply.github.com>
2019-01-30 11:50:36 +0800

    11

--- 20190109data_dic新增部门脚本.sql
+++
@@ -1,7 +0,0 @@
-prompt Importing table Data_Dic...
-set feedback off
-set define off
-insert into Data_Dic (ID, DATA_TYPE_NO, DATA_NO, DATA_TYPE_NAME, DATA_NO_LEN, DATA_NAME, LIMIT_FLAG, HIGH_LIMIT, LOW_LIMIT, EFFECT_DATE, EXPIRE_DATE, TIMESTAMPS, MISCFLGS, APPROVE_STATUS, APPROVE_RESULT, REC_STATUS, REP_STATUS, IS_SUB_SUCCESS, CRT_TM, LST_UPD_TM, LST_UPD_TLR, APPTYPE, BR_NO, YWDATE, ORGCODE, RECORD_UPD_TLR, RECORD_UPD_TM, ST)
-values (214, 20000, 'CPD', '≤ø√≈', null, 'CPD', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, '1');
-
-prompt Done.
